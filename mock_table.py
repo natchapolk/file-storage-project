53a49908ef7e4b02f74697fd81a4db9adc4cb308
natchapolk <tactiledev.nat@gmail.com>
2022-10-05 15:58:38 +0700

    create mock sqlite database

---
+++ mock_table.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+# Connect to database test.sqlite
+conn = sqlite3.connect("test.sqlite")
+
+#Clear all data
+conn.execute("""DROP TABLE files;""")
+conn.execute("""DROP TABLE users;""")
+
+# Create table Files
+conn.execute("""CREATE TABLE FILES(
+FileID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+NAME VARCHAR NOT NULL,
+TYPE VARCHAR NOT NULL,
+PATH VARCHAR NOT NULL,
+UserID INTEGER NOT NULL,
+FOREIGN KEY(UserID) REFERENCES USERS(UserID)
+);""")
+
+# Create table Users
+conn.execute("""CREATE TABLE USERS(
+UserID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+USERNAME VARCHAR NOT NULL,
+PASSWORD VARCHAR NOT NULL
+);""")
+
+# Close connection
+conn.close()
