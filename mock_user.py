bd9b2208ad704ea9eb0a6c887c7d984aa604f4b5
natchapolk <tactiledev.nat@gmail.com>
2022-10-06 09:18:43 +0700

    Mock user and password by get  username and password from users.txt file.

---
+++ mock_user.py
@@ -0,0 +1,20 @@
+import os
+import sqlite3
+# Connect to database
+conn = sqlite3.connect("test.sqlite")
+#Get user and password list
+f = open("users.txt", "r")
+data = f.read()
+f.close()
+data = data.split("\n")
+for user in data:
+	user = user.split("\t")
+	print(user[0]+" "+user[1])
+	#create folder
+	try: os.mkdir("files/"+user[0])
+	except: print("Folder exist")
+	#Add user and password to database
+	conn.execute("INSERT INTO USERS(USERNAME, PASSWORD) VALUES ('"+user[0]+"', '"+user[1]+"');")
+conn.commit()
+# Close connection
+conn.close()
