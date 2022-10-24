1372daeccc38f438766c782172778742e56b583d
natchapolk <tactiledev.nat@gmail.com>
2022-10-25 01:35:28 +0700

    Add function upload file to frontend.

--- web/util.js
+++ web/util.js
@@ -2,3 +2,26 @@ function handleLogout(){
 window.localStorage.removeItem("DES424");
 window.location.href = "index.html";
 }
+
+function handleUploadFile(){
+var data = new FormData()
+data.append('files', document.getElementById("file").files[0]);
+var path = "/file";
+var option = {
+method: 'POST',
+headers: {
+'Authorization': 'Bearer '+window.localStorage.getItem("DES424")
+},
+body: data
+}
+fetch(path, option)
+.then(function(response){
+if (response.status == 201){
+response.json().then(function(json){
+alert(json['result']);
+document.getElementById("file").value = null;
+})
+return;
+}
+})
+}
